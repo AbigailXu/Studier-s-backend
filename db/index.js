b7d88f2d0695130e7931b6a7eb0cea5ee32ea4f4
AbigailXu <qbxzz233@gmail.com>
2021-05-25 01:52:17 -0400

    make it better

--- db/index.js
+++ db/index.js
@@ -1,9 +1,8 @@
-//mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb
-
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/studiers-db', { useNewUrlParser: true })
+    .connect('mongodb+srv://Abigail:abab@cluster0.rvq7g.mongodb.net/studier-s?retryWrites=true&w=majority', { useNewUrlParser: true })
+    //old:  mongodb://127.0.0.1:27017/studiers-db
     .catch(e => {
         console.error('Connection error', e.message)
     })
