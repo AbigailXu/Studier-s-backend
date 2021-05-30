c37bb441402c200060473c8749f8b308930e4199
AbigailXu <qbxzz233@gmail.com>
2021-05-30 06:58:23 -0400

    pm2

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT;
+const port =5000; //process.env.PORT;
 
 // MongoDB
 const db = require("./db");
 const taskRouter = require('./routes/task-router')
 const genreRouter = require('./routes/genre-router')
-
-
+//const youtubeRouter = require('./routes/youtube-router')
 
 
 app.use(express.json()); //Used to parse JSON bodies
@@ -27,6 +26,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/genre", genreRouter);
 app.use("/api/task", taskRouter);
+//app.use("/api/youtube", youtubeRouter);
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
